338fb682f47a75fd58e051108e422247ec6188ce
Tran Nhat Dat <trannhatdat93lhp@gmail.com>
2022-11-29 10:20:06 +0700

    update

--- src/main/java/Common/Constant.java
+++ src/main/java/Common/Constant.java
@@ -1,4 +1,10 @@
 package Common;
 
+import org.openqa.selenium.WebDriver;
+
 public class Constant {
+    public static WebDriver WEBDRIVER;
+    public static final String TA_DASHBOARD_URL = "http://localhost/TADashboard/login.jsp";
+    public static final String USERNAME = "administrator";
+    public static final String PASSWORD = "";
 }
--- src/main/java/Common/Utilities.java
+++ src/main/java/Common/Utilities.java
@@ -1,4 +1,8 @@
 package Common;
 
 public class Utilities {
+    public static String getProjectPath() {
+        String currentDirectory = System.getProperty("user.dir");
+        return currentDirectory + "/src/main/java/";
+    }
 }
