ad5c88bcc076777aca35c9bd3eaa9a53abfce6d4
maivuhoa <hoavumai1996@gmail.com>
2022-11-30 10:32:18 +0700

    update tc10

--- src/main/java/Testcases/DA_LOGIN/Testcase010.java
+++ src/main/java/Testcases/DA_LOGIN/Testcase010.java
@@ -1,17 +1,23 @@
 package Testcases.DA_LOGIN;
 
+import Common.DriverManager;
 import DataObjects.User;
 import PageObjects.LoginPage;
 import Testcases.BaseTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.logging.LogManager;
-
 public class Testcase010 extends BaseTest {
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.LogManager.getLogger(Testcase010.class);
     @Test
-    public void DA_LOGIN_TC010(){
+    public void DA_LOGIN_TC010() {
         LoginPage loginPage = new LoginPage();
-        User user = new User("","");
+        User user = new User("", "");
+        logger.info("Login with blank username and password");
         loginPage.login(user);
+        Assert.assertTrue(DriverManager.isAlertPresent());
     }
 }
