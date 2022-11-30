bc1a48a52cedf63de148c1d4ef4a3b957be3cd33
maivuhoa <hoavumai1996@gmail.com>
2022-11-30 09:43:04 +0700

    update

--- src/main/java/Testcases/DA_LOGIN/Testcase005.java
+++ src/main/java/Testcases/DA_LOGIN/Testcase005.java
@@ -4,6 +4,5 @@ import Testcases.BaseTest;
 import org.testng.annotations.Test;
 
 public class Testcase005 extends BaseTest {
-    @Test
 
 }
--- src/main/java/Testcases/DA_LOGIN/Testcase010.java
+++ src/main/java/Testcases/DA_LOGIN/Testcase010.java
@@ -1,5 +1,17 @@
-package Testcases;
+package Testcases.DA_LOGIN;
 
-public class Testcase010 extends BaseTest{
+import DataObjects.User;
+import PageObjects.LoginPage;
+import Testcases.BaseTest;
+import org.testng.annotations.Test;
 
+import java.util.logging.LogManager;
+
+public class Testcase010 extends BaseTest {
+    @Test
+    public void DA_LOGIN_TC010(){
+        LoginPage loginPage = new LoginPage();
+        User user = new User("","");
+        loginPage.login();
+    }
 }
